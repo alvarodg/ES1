a54c4f58500ed8c171cb3e6b74e49f2b82721b3c
Alvaro Duran <alvaroduran.96@gmail.com>
2018-04-13 19:22:32 +0200

    Añadiendo saludos en castellano

--- castellano.py
+++ castellano.py
@@ -1,4 +1,8 @@
 def saludar():
     print("Hola mundo!")
+    print("¡Hola todo el mundo!")
+    print("¡Hola Doctor Nick!")
+    print("¿Qué tal?")
+    print("¡Hombre!,¿dónde te metes?")
     return
 saludar()
